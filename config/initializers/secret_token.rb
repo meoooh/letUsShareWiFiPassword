724ef2aa9cab3fe195b7064b17c7bf392295bec9
han <hgk617@naver.com>
2013-06-16 09:13:14 -0700

    배포 설정 완료

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetUsShareWiFiPassword::Application.config.secret_token = '36ca48db76eabde18a67347c58b811a1a4d125f881f18b46a814f4caa81b321e7654829ae531659af75ef6d963a6b0bfa61c9b68191c9e0add0051cc13bff659'
